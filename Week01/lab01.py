3d45011343f2f7819c1993bab49ed6f134a6386a
SaKsHaTGaRg <sakshat2422@gmail.com>
2025-01-14 18:04:42 -0500

    Lab01

--- Week01/lab01.py
+++ Week01/lab01.py
@@ -1,1 +1,26 @@
-print("HI")
+# Sample Coding  Questions 01 Week 01
+# Sakshat Garg
+
+# defining variable
+
+array = [1,4,7,9]
+
+#Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+#pedmas
+# fully bracketed version of e :
+e = a-((b**c)//d) + (a%c)
+
+
+#Formatting
+temperature = 32.6
+print(f"The temperature today is: {temperature:.3f} degrees Celsius")
+
+
+#Common Function
+userAge = int(input("What is your age?"))
+userAge = userAge+22
+print("Now showing the shop items filtered by age:" , userAge)
