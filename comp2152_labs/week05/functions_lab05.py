9420eaa1288615401f1d760d0919707ada7fbe4f
SaKsHaTGaRg <sakshat2422@gmail.com>
2025-02-04 11:08:19 -0500

    add Q3 solution

---
+++ comp2152_labs/week05/functions_lab05.py
@@ -0,0 +1,12 @@
+import random
+
+
+
+
+# question 3
+def collect_loot(loot_options , belt):
+    print("Random loot process")
+    loot_roll = random.choice(range(1,len(loot_options) +1 ))
+    loot = loot_options.pop(loot_roll - 1)
+    print(" Your belt :" + belt)
+    return loot,belt
