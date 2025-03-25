4d3565dc4bfcafb112077d1b61614c174f16dd1e
SaKsHaTGaRg <sakshat2422@gmail.com>
2025-03-25 10:54:45 -0400

    add all the classes

---
+++ comp2152_labs/week12/heart.py
@@ -0,0 +1,8 @@
+# Heart class for composition
+class Heart:
+    def __init__(self):
+        print("Compostion: Heart is created")
+        self.beats_per_minute = 72
+
+    def beat(self):
+        print("Heart is beating")
---
+++ comp2152_labs/week12/tick.py
@@ -0,0 +1,11 @@
+# Tick class for aggregation
+class Tick:
+    def __init__(self):
+        print("Aggregation: Tick is created")
+
+    def consume_blood(self):
+        print("Tick is consuming blood")
+
+
+    def __del__(self):
+        print("Aggregation: Tick is destroyed")
